28c69ad3dabc6afea0278a32f818e5aedaf97938
Lalin Jha <130792197+jhalalin@users.noreply.github.com>
2025-03-19 02:41:10 +0530

    Delete Automation Project directory

--- Automation Project/src/main/java/flipkart/config/BrowserStackConfig.java
+++
@@ -1,9 +0,0 @@
-package flipkart.config;
-
-public class BrowserStackConfig {
-    public static final String USERNAME = "your_browserstack_username";
-    public static final String ACCESS_KEY = "your_browserstack_access_key";
-    public static final String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@hub-cloud.browserstack.com/wd/hub";
-    public static final String AUTOMATE_KEY = "your_automate_key";
-    public static final boolean LOCAL_TESTING = false; // Set true if testing local application
-}
--- Automation Project/src/main/java/flipkart/config/Config.java
+++
@@ -1,8 +0,0 @@
-package flipkart.config;
-
-public class Config {
-  public static final String BASE_URL = "https://www.flipkart.com";
-  public static final String CHROME_DRIVER_PATH = "path/to/chromedriver";
-  public static final int PAGE_LOAD_TIMEOUT = 30;
-  public static final int IMPLICIT_WAIT = 10;
-}
