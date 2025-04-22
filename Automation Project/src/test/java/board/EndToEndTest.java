3aa52757defd27bda34621a4669031224d6de357
Lalin Jha <130792197+jhalalin@users.noreply.github.com>
2025-04-23 01:51:54 +0530

    Update EndToEndTest.java

--- Automation Project/src/test/java/board/EndToEndTest.java
+++ Automation Project/src/test/java/board/EndToEndTest.java
@@ -10,15 +10,6 @@ public class EndToEndTest extends BaseTest {
     // Login
     LoginPage loginPage = new LoginPage(driver);
     HomePage homePage = loginPage.login("your_username", "your_password");
-    // Search product
-    LogoutPage searchResults = homePage.searchProduct("iPhone 13");
-    // Select product
-    SettingsPage settingsPage = searchResults.selectFirstProduct();
-    // Add to cart
-    BoardPage boardPage = settingsPage.addToCart();
-    // Verify cart page is displayed
-    Assert.assertTrue(boardPage.placeOrderButton.isDisplayed(), "Cart page not displayed correctly");
-    // Proceed to checkout
     boardPage.placeOrder();
   }
 }
