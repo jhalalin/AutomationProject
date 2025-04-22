7e747197003e6987b17192632de00a46129eaeee
Lalin Jha <130792197+jhalalin@users.noreply.github.com>
2025-04-23 01:52:44 +0530

    Delete Automation Project directory

--- Automation Project/src/main/java/board/config/BrowserStackConfig.java
+++
@@ -1,9 +0,0 @@
-package board.config;
-
-public class BrowserStackConfig {
-    public static final String USERNAME = "lalinjha_OiAebd";
-    public static final String ACCESS_KEY = "W8d4nkCNz9hJ5USLxqCy";
-    public static final String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@hub-cloud.browserstack.com/wd/hub";
-    public static final String AUTOMATE_KEY = "your_automate_key";
-    public static final boolean LOCAL_TESTING = false; // Set true if testing local application
-}
